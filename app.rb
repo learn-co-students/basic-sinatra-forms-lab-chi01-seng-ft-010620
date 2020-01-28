e56c1da7c2831a5771926657e8cb0b84c8bdc0fc
github username <github email address>
2020-01-28 13:07:33 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,27 @@
 require 'sinatra/base'
+require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/newteam' do 
+        erb :newteam
+    end 
+
+
+    post '/team' do 
+        # binding.pry
+        @submission = params
+
+        erb :team
+    end
+
+    # get '/team' do 
+    #     erb :team
+    # end
+
+    # get '/team' do 
+    #     erb :team
+    # end
+
 
 
 end
