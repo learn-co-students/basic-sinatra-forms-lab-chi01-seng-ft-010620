6b8235b3a7b471d8e074c42735d81a7aa3773cea
Niko Dixon <nikodixon@gmail.com>
2020-01-28 15:48:10 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
+
+
+    get '/newteam' do 
+        erb :newteam 
+    end 
+
+    post '/team' do 
+        @team_name = params["name"]
+        @coach = params["coach"]
+        @pg = params["pg"]
+        @sg = params["sg"]
+        @pf = params["pf"]
+        @c = params["c"]
+        erb :team 
+        
+    end 
 
 
 end
